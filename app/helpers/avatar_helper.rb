94d993ffb6c63d1ead9c13c0ed4ed01d309dd644
Luis Cesar <lutzcc1@gmail.com>
2020-11-20 20:56:37 -0600

    move avatar helper method to AvatarHelper

---
+++ app/helpers/avatar_helper.rb
@@ -0,0 +1,9 @@
+module AvatarHelper
+
+  def avatar(user)
+    email_digest = Digest::MD5.hexdigest(user.email)
+    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
+
+    image_tag gravatar_url
+  end
+end
