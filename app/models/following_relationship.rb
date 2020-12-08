1d0c0e69d003113f2067c65c8a678b71d2de220d
Luis Cesar <lutzcc1@gmail.com>
2020-12-08 12:55:08 -0600

    enable users following

--- app/models/following_relationship.rb
+++ app/models/following_relationship.rb
@@ -1,6 +1,6 @@
 class FollowingRelationship < ApplicationRecord
-  belongs_to :follower, class_name: "User"
-  belongs_to :followed_user, class_name: "User"
+  belongs_to :follower, class_name: "User", counter_cache: :followed_users_count
+  belongs_to :followed_user, class_name: "User", counter_cache: :followers_count
 
   validates :follower_id, uniqueness: { scope: :followed_user_id }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+mewtwo = User.create!(username: 'mewtwo', email: 'mewtwo@gmail.com', password: 'contraseña')
+telos = User.create!(username: 'telos', email: 'telos@gmail.com', password: 'contraseña')
+ganon = User.create!(username: 'ganon', email: 'ganondorf@gmail.com', password: 'contraseña')
+
+Shout.create!(
+    user: mewtwo,
+    content: TextShout.create!(body: "I see now that the circumstances of one's birth are irrelevant. It is what you do with the gift of life that determines who you are")
+)
+
+Shout.create!(
+    user: telos,
+    content: TextShout.create!(body: "I was created by a 🌱")
+)
+
+Shout.create!(
+    user: ganon,
+    content: TextShout.create!(body: "DORIYAHHHHH")
+)
