a8a627b932a9c9bc8f000eea674b782e80f1164a
Luis Cesar <lutzcc1@gmail.com>
2020-12-22 17:58:27 -0600

    add shout_form_for helper

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -1,5 +1,13 @@
 module ShoutHelper
 
+  def shout_form_for(content_type)
+    form_for Shout.new do |f|
+      f.hidden_field(:content_type, value: content_type) +
+      f.fields_for(:content) { |content_form| yield(content_form) } +
+      f.submit("Shout!")
+    end
+  end
+
   def like_button(shout)
     if current_user.liked? shout
       link_to "unlike", unlike_shout_path(shout), method: :delete
